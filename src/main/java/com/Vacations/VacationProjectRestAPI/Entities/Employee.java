005a70f3916c02f9cabb7b59c670786629bcbc58
Arnoldas <arnoldaspetrauskas112@gmail.com>
2023-05-04 22:00:33 +0300

    Made many to many relationship between vacation and employee

--- src/main/java/com/Vacations/VacationProjectRestAPI/Entities/Employee.java
+++ src/main/java/com/Vacations/VacationProjectRestAPI/Entities/Employee.java
@@ -1,2 +1,29 @@
-package com.Vacations.VacationProjectRestAPI.Entities;public class Employee {
+package com.Vacations.VacationProjectRestAPI.Entities;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Employee {
+
+    @NonNull
+    private Integer id;
+
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String role;
+
+    private Set<Vacation> vacations;
 }
